141122760778f39ae920e6079f864e4c4204cd9e
BabSchu <you@example.com>
2019-11-12 10:51:11 +0100

    finish exs 1

---
+++ Exc1/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <?php
+        $viewer = getenv( "HTTP_USER_AGENT" );
+        $css = "";
+
+        if (preg_match('/Chrome/i' , "$viewer")) {
+            $css = 'chrome.css';
+        } elseif (preg_match( "/Mozilla/i", "$viewer")) {
+            $css = "mozilla.css";
+        }
+    ?>
+
+    <?= "<link rel='stylesheet' type='text/css' href=$css>" ?>
+  
+    <title>PHP</title>
+</head>
+
+<body >
+
+
+    <div id="box"></div>
+
+</ body>
+</html>
